78d2a2be771bdf5f8eeff1061bafcca9e78f71b9
HuyVo <huyvo6812@gmail.com>
2023-11-03 13:47:22 +0700

    add on vectordb chroma, document loader for pdf

--- app/model/prompt_template.py
+++ app/model/prompt_template.py
@@ -18,3 +18,90 @@ Question: {question}
 
 Answer: <your answer here>
 """
+
+GENERATE_JOB_DESCRIPTION_AND_QUESTIONS_PROMPT = """
+You are a Senior Human Resource Executive at {company_name}. Your role is to read a job description and the company background information, and act like you are a candidate who is interested in the role. Your goal is to come up with a list of questions that you would ask the Human Resources team to clarify more about the role.
+
+Given the following information about {company_name} and the role they are currently hiring for, you need to come up with a list of at least 20 questions that you want to ask along with the answer for each question. These questions should cover the following areas:
+
+1. Role description: Ask questions about the responsibilities and duties of the role, as well as the expected outcomes.
+
+2. Requirements: Ask questions about the qualifications, skills, and experience required for the role.
+
+3. Company culture: Ask questions about the company's values, mission, and work environment.
+
+4. Benefits: Ask questions about the benefits and perks of the role, such as health insurance, retirement plans, and vacation time.
+
+5. Career development: Ask questions about opportunities for growth and advancement within the company.
+
+Your questions must be clear, concise, and easy to understand. Avoid asking questions that can be easily answered by reading the job description or the company's website.
+
+Here's an example of a good question: "What are the main responsibilities of this role, and how do they contribute to the company's overall goals?"
+
+Here's an example of a bad question: "What does this company do?"
+
+Job Description:
+{job_description}
+
+Company Background information:
+{company_background_info}
+
+examples:
+1. Q: What are the main responsibilities of this role, and how do they contribute to the company's overall goals?
+   A: The main responsibilities of this role are X, Y, and Z. These responsibilities contribute to the company's overall goals by A, B, and C.
+
+2. Q: What are the required qualifications for this role?
+   A: The required qualifications for this role are X, Y, and Z.
+
+3. Q: What is the company culture like?
+   A: The company culture is focused on X, Y, and Z. We value A, B, and C.
+
+4. Q: What benefits and perks are offered with this role?
+   A: We offer health insurance, retirement plans, and vacation time.
+
+5. Q: What opportunities are there for growth and advancement within the company?
+   A: There are opportunities for growth and advancement within the company through X, Y, and Z.
+
+answers:
+List of question and answer:
+1:  Q: <YOUR QUESTION AT HERE>
+    A: <YOUR ANSWER AT HERE>
+2:  Q: <YOUR QUESTION AT HERE>
+    A: <YOUR ANSWER AT HERE>
+3:  Q: <YOUR QUESTION AT HERE>
+    A: <YOUR ANSWER AT HERE>
+4:  Q: <YOUR QUESTION AT HERE>
+    A: <YOUR ANSWER AT HERE>
+5:  Q: <YOUR QUESTION AT HERE>
+    A: <YOUR ANSWER AT HERE>
+6:  Q: <YOUR QUESTION AT HERE>
+    A: <YOUR ANSWER AT HERE>
+7:  Q: <YOUR QUESTION AT HERE>
+    A: <YOUR ANSWER AT HERE>
+8:  Q: <YOUR QUESTION AT HERE>
+    A: <YOUR ANSWER AT HERE>
+9:  Q: <YOUR QUESTION AT HERE>
+    A: <YOUR ANSWER AT HERE>
+10:  Q: <YOUR QUESTION AT HERE>
+    A: <YOUR ANSWER AT HERE>
+11:  Q: <YOUR QUESTION AT HERE>
+    A: <YOUR ANSWER AT HERE>
+12:  Q: <YOUR QUESTION AT HERE>
+    A: <YOUR ANSWER AT HERE>
+13:  Q: <YOUR QUESTION AT HERE>
+    A: <YOUR ANSWER AT HERE>
+14:  Q: <YOUR QUESTION AT HERE>
+    A: <YOUR ANSWER AT HERE>
+15:  Q: <YOUR QUESTION AT HERE>
+    A: <YOUR ANSWER AT HERE>
+16:  Q: <YOUR QUESTION AT HERE>
+    A: <YOUR ANSWER AT HERE>
+17:  Q: <YOUR QUESTION AT HERE>
+    A: <YOUR ANSWER AT HERE>
+18:  Q: <YOUR QUESTION AT HERE>
+    A: <YOUR ANSWER AT HERE>
+19:  Q: <YOUR QUESTION AT HERE>
+    A: <YOUR ANSWER AT HERE>
+20:  Q: <YOUR QUESTION AT HERE>
+    A: <YOUR ANSWER AT HERE>
+"""
--- client/interface.py
+++ client/interface.py
@@ -2,8 +2,25 @@
 from __future__ import annotations
 
 import streamlit as st
+import chromadb
 
-from client.partials import material_sidebar
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+
+
+# from client.partials import material_sidebar
+from app.src.document_loader import PDFDocumentLoader
+
+VECTOR_DB = chromadb.PersistentClient(path="database/vectordb")
+# collection = vectordb.get_or_create_collection(name="test")
+EMBEDDING_FUNC = OpenAIEmbeddings(
+    openai_api_key=st.secrets["OPENAI_API_KEY"],
+    model_name="text-embedding-ada-002",
+)
+
+LANGCHAIN_VECTOR_DB = Chroma(client=VECTOR_DB, collection_name="test", embedding_function=EMBEDDING_FUNC)
+
+PDF_LOADER = PDFDocumentLoader(vector_db=LANGCHAIN_VECTOR_DB, embedding_model=EMBEDDING_FUNC, text_splitter=None)
 
 
 def main_interface():
@@ -15,5 +32,12 @@ def main_interface():
     if "messages" not in st.session_state:
         st.session_state["messages"] = []
 
-    with st.sidebar:
-        temperature = material_sidebar.display_temperature_slider()
+    uploaded_file = st.file_uploader("Choose a PDF file", type="pdf", accept_multiple_files=True)
+    if uploaded_file is not None:
+        for data in uploaded_file:
+            bytes_data = data.getvalue()
+            st.write("Read Document")
+            docs = PDF_LOADER.load_document(bytes_data, data.name)
+            st.write("Push to DB")
+            PDF_LOADER.upload_to_db(docs)
+            st.write("Done")
